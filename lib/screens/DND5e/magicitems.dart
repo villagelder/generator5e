0eb3c444efa17ddf5c5d605444be77f7d3760e73
mleopin <matthewleopin@gmail.com>
2023-10-31 18:02:24 -0400

    starting magic item generator

--- lib/screens/DND5e/magicitems.dart
+++ lib/screens/DND5e/magicitems.dart
@@ -1,41 +1,260 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import '../../services/treasureGenerator5e.dart';
 
 class MagicItemsPage extends MaterialPageRoute<Null> {
   MagicItemsPage()
       : super(builder: (BuildContext ctx) {
           return Scaffold(
-            backgroundColor: const Color.fromRGBO(210, 210, 172, 1.0),
+            backgroundColor: Colors.blueGrey.shade100,
             appBar: AppBar(
               title: const Text(
                 'DragonVault',
                 style: TextStyle(
-                  color: Color.fromRGBO(210, 210, 172, 1.0),
+                  fontFamily: 'Georgia',
+                  color: Color.fromRGBO(255, 245, 188, 1.0),
                 ),
               ),
-              backgroundColor: const Color.fromRGBO(34, 56, 69, 1.0),
+              backgroundColor: const Color.fromRGBO(57, 0, 0, 1.0),
             ),
-            body: Center(
-              child: ListView(
-                children: const [
-                  Padding(
-                    padding: EdgeInsets.all(12.0),
+            body: SingleChildScrollView(
+              scrollDirection: Axis.vertical,
+              child: Column(
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.fromLTRB(0, 12.0, 0, 0),
                     child: SizedBox(
                       height: 48,
                       child: Center(
                         child: Text(
-                          '5e Magic Item Generator',
+                          '5e Magic Items',
                           style: TextStyle(
+                            fontFamily: 'Georgia',
                             fontSize: 32,
+                            fontWeight: FontWeight.w500,
                             color: Color.fromRGBO(34, 56, 69, 1.0),
                           ),
                         ),
                       ),
                     ),
                   ),
+                  SizedBox(
+                    width: MediaQuery.of(ctx).size.width * 0.84,
+                    child: const Padding(
+                      padding: EdgeInsets.fromLTRB(0, 12.0, 0, 12.0),
+                      child: Text(
+                        'Select your magic item Rarity and Type, then push \'Generate Magic Item\' to '
+                        'generate new magic items.',
+                        style: TextStyle(
+                            fontFamily: 'Georgia', fontStyle: FontStyle.italic),
+                      ),
+                    ),
+                  ),
+                  const TextChanger(),
                 ],
               ),
             ),
           );
         });
+}
+
+class ItemTypeDDB extends StatefulWidget {
+  const ItemTypeDDB({super.key});
+
+  @override
+  State<ItemTypeDDB> createState() => _ItemTypeDDBState();
+}
+
+class RarityDDB extends StatefulWidget {
+  const RarityDDB({super.key});
+
+  @override
+  State<RarityDDB> createState() => _RarityDDBState();
+}
+
+const List<String> rarityList = <String>[
+  'CR 0-4',
+  'CR 5-10',
+  'CR 11-16',
+  'CR 17+'
+];
+const List<String> itemTypeList = <String>[
+  'Common',
+  'Uncommon',
+  'Rare',
+  'Very Rare',
+  'Legendary'
+];
+
+class _RarityDDBState extends State<RarityDDB> {
+  static String rarityValue = rarityList.first;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15.0),
+        border: Border.all(
+            color: const Color.fromRGBO(38, 50, 56, 1.0),
+            style: BorderStyle.solid,
+            width: 2.0),
+      ),
+      width: MediaQuery.of(context).size.width * 0.4,
+      child: DropdownButtonHideUnderline(
+        child: ButtonTheme(
+          alignedDropdown: true,
+          child: DropdownButton<String>(
+            value: rarityValue,
+            icon: const Icon(Icons.arrow_drop_down_outlined),
+            elevation: 16,
+            style: const TextStyle(
+              color: Color.fromRGBO(38, 50, 56, 1.0),
+              fontFamily: 'Georgia',
+              fontSize: 20,
+              fontWeight: FontWeight.w500,
+            ),
+            underline: Container(
+              height: 2,
+              color: Colors.blueGrey,
+            ),
+            onChanged: (String? value) {
+              // This is called when the user selects an item.
+              setState(() {
+                rarityValue = value!;
+              });
+            },
+            items: rarityList.map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _ItemTypeDDBState extends State<ItemTypeDDB> {
+  static String typeValue = itemTypeList.first;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15.0),
+        border: Border.all(
+            color: Colors.brown.shade800, style: BorderStyle.solid, width: 2.0),
+      ),
+      width: MediaQuery.of(context).size.width * 0.4,
+      child: DropdownButtonHideUnderline(
+        child: ButtonTheme(
+          alignedDropdown: true,
+          child: DropdownButton<String>(
+            value: typeValue,
+            icon: const Icon(Icons.arrow_drop_down_outlined),
+            elevation: 16,
+            style: const TextStyle(
+                color: Color.fromRGBO(38, 50, 56, 1.0),
+                fontFamily: 'Georgia',
+                fontSize: 20,
+                fontWeight: FontWeight.w500),
+            underline: Container(
+              height: 2,
+              color: Colors.blueGrey,
+            ),
+            onChanged: (String? value) {
+              // This is called when the user selects an item.
+              setState(() {
+                typeValue = value!;
+              });
+            },
+            items: itemTypeList.map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class TextChanger extends StatefulWidget {
+  const TextChanger({super.key});
+
+  @override
+  State<TextChanger> createState() => _TextChangerState();
+}
+
+class _TextChangerState extends State<TextChanger> {
+  // Declare the variable
+  String dynamicText = 'Roll for magic item.';
+  TreasureGenerator5e trGen = TreasureGenerator5e();
+
+  updateText() {
+    setState(() {
+      // dynamicText = trGen
+      //     .generate(_RarityDDBState.crValue, _TreasureTypeDDBState.ttValue)
+      //     .toString();
+    });
+  }
+
+  @override
+  Widget build(BuildContext ctx) {
+    return Column(
+      children: [
+        const Padding(
+          padding: EdgeInsets.all(12.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              // ChallengeRatingDDB(),
+              // TreasureTypeDDB(),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(0, 20.0, 0, 0),
+          child: SizedBox(
+            width: MediaQuery.of(ctx).size.width * 0.84,
+            height: MediaQuery.of(ctx).size.height * 0.1,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                foregroundColor: const Color.fromRGBO(255, 245, 188, 1.0),
+                backgroundColor: Colors.lightGreen.shade900,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+              ),
+              onPressed: () => updateText(),
+              child: const Text(
+                'Generate Magic Item',
+                style: TextStyle(
+                    fontFamily: 'Georgia',
+                    fontSize: 24,
+                    fontWeight: FontWeight.w600),
+              ),
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(40.0),
+          child: Text(dynamicText,
+              style: const TextStyle(fontFamily: 'Georgia', fontSize: 16)),
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(0, 0, 0, 20.0),
+          child: Icon(
+            Icons.copy,
+            color: Colors.blueGrey.shade900,
+            size: 40.0,
+          ),
+        ),
+      ],
+    );
+  }
 }
